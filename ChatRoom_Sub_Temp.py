5068f3970e05150f117a63d79ed1774fa8265684
Lena Fox <35292184+TheLenaFox@users.noreply.github.com>
2024-05-15 09:35:23 +0200

    Updated city location query

--- ChatRoom_Sub_Temp.py
+++ ChatRoom_Sub_Temp.py
@@ -6,21 +6,23 @@ port = int(80)
 import requests
 import time
 
-def get_external_temperature(city):
+global weather_location
+weather_location = input("Which city for weather data? ")
+
+def get_external_temperature():
     api_key = "4b608b026eabe79968f2bfe9e0c8b34f"
-    weather_location = "Toronto"
     base_url = f"http://api.openweathermap.org/data/2.5/weather?q={weather_location}&appid={api_key}&units=metric"
     response = requests.get(base_url)
     data = response.json()
-    print(data)
+    #print(data)
     return data['main']['temp']
 
 print('Connecting to ', host, port)
 s.connect((host, port))
 
 while True:
-  temperature = get_external_temperature("Tokyo")
-  msg = str(temperature)
+  temperature = get_external_temperature()
+  msg = str(f"Temperature is: {temperature} in {weather_location}")
   msg = msg.encode()
   s.send(msg)
   time.sleep(5)
